6340dc20f637ffb39b352d83ff2f2f01fc32adbb
Fabián Espitia <fabianespitia370@gmail.com>
2024-07-09 09:42:54 -0500

    hero content modified

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,18 +2,20 @@ import { Fade } from "react-awesome-reveal";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-[calc(100vh-14rem)]">
+    <section className="relative min-h-screen flex items-center">
       <Fade triggerOnce cascade duration={900}>
-        <h1 className="text-4xl md:text-7xl md:mb-4 md:pl-8 font-extrabold text-gray-900">
-          Learn To <span className="text-gradient-2">Invest</span> In{" "}
-          <span className="text-gradient-2">Startups</span> Like The{" "}
-          <span className="text-gradient-2">Best In Latam</span>
-        </h1>
-        <p className="text-base md:text-xl md:pl-11 pt-4 text-gray-600 max-w-3xl">
-          To empower both aspiring and seasoned investors with the knowledge,
-          tools, and network needed to identify and invest in the most promising
-          startups in Latin America.
-        </p>
+        <div className="text-start">
+          <h1 className="text-4xl md:text-7xl md:mb-4 font-extrabold text-gray-900 md:pl-8">
+            Learn To <span className="text-gradient-2">Invest</span> In{" "}
+            <span className="text-gradient-2">Startups</span> Like The{" "}
+            <span className="text-gradient-2">Best In Latam</span>
+          </h1>
+          <p className="text-base md:text-xl pt-4 text-gray-600 max-w-3xl md:pl-11">
+            To empower both aspiring and seasoned investors with the knowledge,
+            tools, and network needed to identify and invest in the most
+            promising startups in Latin America.
+          </p>
+        </div>
       </Fade>
     </section>
   );
